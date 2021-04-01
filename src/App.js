3ec72f5940902ab8372911dcd12a8ce38dda19b7
mor <mor6686@gmail.com>
2021-04-02 01:59:23 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import axios from 'axios'
+// import React, { useRef } from 'react'
+function App() {
+
+
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://bing-image-search1.p.rapidapi.com/images/search',
+  //   params: { q: 'vineyard' },
+  //   headers: {
+  //     'x-rapidapi-key': '8edafba73bmshfecc6ee28745328p10bcf9jsn5a7b93532125',
+  //     'x-rapidapi-host': 'bing-image-search1.p.rapidapi.com'
+  //   }
+  // };
+
+  // axios.request(options).then(function (response) {
+  //   console.log(response.data);
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
+  return (
+    <div className="App">
+      <h1>
+        The site is under construction
+      </h1>
+    </div>
+  );
+}
+
+export default App;
