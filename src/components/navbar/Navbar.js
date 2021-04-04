cfcdb81d1d7e366aadd1e6f88521c320e53a3da7
mor <mor6686@gmail.com>
2021-04-04 03:26:53 +0300

    mid-project_0.5

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
+import {
+    BrowserRouter as Router,
+    Link,
+} from 'react-router-dom'
+
 import './navbar.css';
+
 import UseAnimations from "react-useanimations";
 import menu3 from 'react-useanimations/lib/menu3'
+
+
 const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
@@ -15,19 +23,21 @@ const Navbar = () => {
     }
 
     return (
-        <div className="my-nav">
-            <a className="menu-logo" href="/">
-                J<span>unior</span>
-            </a>
-            <div className="hamburger" onClick={() => { setIsOpen(!isOpen) }}>
-                <UseAnimations animation={menu3} />
-            </div>
-            <div className="nav-menu" style={{ maxHeight: menuOpen() }}>
-                <a className="menu-link" href="/">Home</a>
-                <a className="menu-link" href="/posts">Posts</a>
-                <a className="menu-link" href="/my-account">My Account</a>
-            </div>
-        </div >
+        <Router>
+            <div className="my-nav">
+                <a className="menu-logo" href="/">
+                    J<span>unior</span>
+                </a>
+                <div className="hamburger" onClick={() => { setIsOpen(!isOpen) }}>
+                    <UseAnimations animation={menu3} />
+                </div>
+                <div className="nav-menu" style={{ maxHeight: menuOpen() }}>
+                    <Link to='/' className="menu-link">Home</Link>
+                    <Link to='/posts' className="menu-link">Posts</Link>
+                    <Link to='/my-account' className="menu-link">My Account</Link>
+                </div>
+            </div >
+        </Router>
     )
 }
 
