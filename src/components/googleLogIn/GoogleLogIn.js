d715c8d325aede44c00ded3798006deb0a2a33d5
mor <mor6686@gmail.com>
2021-04-08 11:24:26 +0300

    mid-project_2.3

--- src/components/googleLogIn/GoogleLogIn.js
+++ src/components/googleLogIn/GoogleLogIn.js
@@ -9,7 +9,7 @@ const MyGoogleLogIn = ({ setUserlog, setUserDetails }) => {
             // ↓↓↓↓↓↓ local ↓↓↓↓↓↓
             // clientId="202064866705-0icj94b2ehlcladq0p3gt5vn9b86ofrk.apps.googleusercontent.com"
             // ↓↓↓↓↓↓ netlify url ↓↓↓↓↓↓
-            clientId="668980613804-snpjdn3okjivv6l78rvaspbg0k2bp39l.apps.googleusercontent.com"
+            clientId="202064866705-dhodlvt04etfkdglt4pnee1tsg7mjs3i.apps.googleusercontent.com"
             buttonText='Sign in with Google'
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
