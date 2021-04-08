d20d62734887293cc3dd3ee754a7d48cbfb3d4ac
mor <mor6686@gmail.com>
2021-04-08 11:14:45 +0300

    mid-project_2.3

--- src/components/googleLogIn/GoogleLogIn.js
+++ src/components/googleLogIn/GoogleLogIn.js
@@ -9,12 +9,6 @@ const MyGoogleLogIn = ({ setUserlog, setUserDetails }) => {
             // ↓↓↓↓↓↓ local ↓↓↓↓↓↓
             // clientId="202064866705-0icj94b2ehlcladq0p3gt5vn9b86ofrk.apps.googleusercontent.com"
             // ↓↓↓↓↓↓ netlify url ↓↓↓↓↓↓
-            // ↓↓↓↓↓↓ local ↓↓↓↓↓↓
-            // clientId="202064866705-0icj94b2ehlcladq0p3gt5vn9b86ofrk.apps.googleusercontent.com"
-            // ↓↓↓↓↓↓ netlify url ↓↓↓↓↓↓
-            // ↓↓↓↓↓↓ local ↓↓↓↓↓↓
-            // clientId="202064866705-0icj94b2ehlcladq0p3gt5vn9b86ofrk.apps.googleusercontent.com"
-            // ↓↓↓↓↓↓ netlify url ↓↓↓↓↓↓
             clientId="668980613804-snpjdn3okjivv6l78rvaspbg0k2bp39l.apps.googleusercontent.com"
             buttonText='Sign in with Google'
             onSuccess={responseGoogle}
